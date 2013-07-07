eb75987533bc057c1c30e6e6a04775a49490c368
Mike Garrett <m@Mike-is-BLACKLETTER-BOLD.local>
2013-07-06 22:48:44 -0400

    Fixed authentication and getting around the strtolower

--- _add_ons/bluebird/pi.bluebird.php
+++ _add_ons/bluebird/pi.bluebird.php
@@ -1,160 +1,164 @@
 <?php
 
 class Plugin_bluebird extends Plugin {
-		
-    var $meta = array(
-        'name'       => 'Blue Bird',
-        'version'    => '1.0',
-        'author'     => 'Nick Snyder',
-        'author_url' => 'http://fasterhorses.co'
-    );
-    
-    public function index() {
-    	
-    	$bbConfig = Spyc::YAMLLoad('_config/add-ons/bluebird.yaml');
-	    $count  = $this->fetch_param('count', 5, 'is_numeric');
-      $screen_name = $this->fetch_param('screen_name', null);
-      $include_rts  = $this->fetch_param('include_rts', true);
-      $include_entities  = $this->fetch_param('$include_entities', true);
-    
-	       
-			function buildBaseString($baseURI, $method, $params) {
-				$r = array(); 
-				ksort($params); 
-				foreach($params as $key=>$value){
-					$r[] = "$key=" . rawurlencode($value); 
-				}
-				return $method."&" . rawurlencode($baseURI) . '&' . rawurlencode(implode('&', $r)); //return complete base string
+
+	var $meta = array(
+		'name'       => 'Blue Bird',
+		'version'    => '1.0',
+		'author'     => 'Nick Snyder',
+		'author_url' => 'http://fasterhorses.co'
+	);
+
+	public function index() {
+
+		$bbConfig = array();
+		$bbConfig['access_token'] = $this->fetch('access_token', null, null, false, false);
+		$bbConfig['access_token_secret'] = $this->fetch('access_token_secret', null, null, false, false);
+		$bbConfig['consumer_key'] = $this->fetch('consumer_key', null, null, false, false);
+		$bbConfig['consumer_secret'] = $this->fetch('consumer_secret', null, null, false, false);
+
+		$count  = $this->fetchParam('count', 5, 'is_numeric');
+		$screen_name = $this->fetchParam('screen_name', null);
+		$include_rts  = $this->fetchParam('include_rts', true);
+		$include_entities  = $this->fetchParam('$include_entities', true);
+
+
+		function buildBaseString($baseURI, $method, $params) {
+			$r = array();
+			ksort($params);
+			foreach ($params as $key=>$value) {
+				$r[] = "$key=" . rawurlencode($value);
 			}
-	
-			function buildAuthorizationHeader($oauth) {
-				$r = 'Authorization: OAuth ';
-				$values = array();
-				foreach($oauth as $key=>$value)
-				$values[] = "$key=\"" . rawurlencode($value) . "\""; 
-				$r .= implode(', ', $values); 
-				return $r; 
-			}
-	
-			$url = "http://api.twitter.com/1.1/statuses/user_timeline.json";
-			
-			$query = array(
-				'count' => $count,
-				'screen_name' => $screen_name,
-				'include_rts' => $include_rts,
-				'include_entities' => $include_entities
+			return $method."&" . rawurlencode($baseURI) . '&' . rawurlencode(implode('&', $r)); //return complete base string
+		}
+
+		function buildAuthorizationHeader($oauth) {
+			$r = 'Authorization: OAuth ';
+			$values = array();
+			foreach ($oauth as $key=>$value)
+				$values[] = "$key=\"" . rawurlencode($value) . "\"";
+			$r .= implode(', ', $values);
+			return $r;
+		}
+
+		$url = "http://api.twitter.com/1.1/statuses/user_timeline.json";
+
+		$query = array(
+			'count' => $count,
+			'screen_name' => $screen_name,
+			'include_rts' => $include_rts,
+			'include_entities' => $include_entities
+		);
+
+		$oauth_access_token = $bbConfig['access_token'];
+		$oauth_access_token_secret = $bbConfig['access_token_secret'];
+		$consumer_key = $bbConfig['consumer_key'];
+		$consumer_secret = $bbConfig['consumer_secret'];
+
+		$oauth = array(
+			'oauth_consumer_key' => $consumer_key,
+			'oauth_nonce' => time(),
+			'oauth_signature_method' => 'HMAC-SHA1',
+			'oauth_token' => $oauth_access_token,
+			'oauth_timestamp' => time(),
+			'oauth_version' => '1.0'
+		);
+		$base_params = empty($query) ? $oauth : array_merge($query, $oauth);
+		$base_info = buildBaseString($url, 'GET', $base_params);
+		$url = empty($query) ? $url : $url . "?" . http_build_query($query);
+
+		$composite_key = rawurlencode($consumer_secret) . '&' . rawurlencode($oauth_access_token_secret);
+		$oauth_signature = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true));
+		$oauth['oauth_signature'] = $oauth_signature;
+
+		$header = array(buildAuthorizationHeader($oauth), 'Expect:');
+
+		$options = array(
+			CURLOPT_HTTPHEADER => $header,
+			CURLOPT_HEADER => false,
+			CURLOPT_URL => $url,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_SSL_VERIFYPEER => false
+		);
+
+		$feed = curl_init();
+		curl_setopt_array($feed, $options);
+		$json = curl_exec($feed);
+		curl_close($feed);
+
+		$twitter_data = json_decode($json);
+
+		if (isset($twitter_data)) {
+
+			$tweets = theNest($twitter_data);
+
+			$output = array(
+				"tweets" => array(),
+				"user" => array()
 			);
 
-			$oauth_access_token = $bbConfig['access_token'];
-			$oauth_access_token_secret = $bbConfig['access_token_secret'];
-			$consumer_key = $bbConfig['consumer_key'];
-			$consumer_secret = $bbConfig['consumer_secret'];
+			foreach ($tweets as $tweet) {
 
-			$oauth = array(
-				'oauth_consumer_key' => $consumer_key,
-        'oauth_nonce' => time(),
-        'oauth_signature_method' => 'HMAC-SHA1',
-        'oauth_token' => $oauth_access_token,
-        'oauth_timestamp' => time(),
-        'oauth_version' => '1.0'
-      );
+				$tweetText = $tweet['text'];
 
-      $base_params = empty($query) ? $oauth : array_merge($query,$oauth);
-      $base_info = buildBaseString($url, 'GET', $base_params);
-      $url = empty($query) ? $url : $url . "?" . http_build_query($query);
+				if (isset($tweet['entities'])) {
+					if (is_array($tweet['entities']['urls'])) { $entityUrl = $tweet['entities']['urls']; }
+					if (is_array($tweet['entities']['hashtags'])) { $entityHash = $tweet['entities']['hashtags']; }
+					if (is_array($tweet['entities']['user_mentions'])) { $entityUser = $tweet['entities']['user_mentions']; }
+				}
 
-			$composite_key = rawurlencode($consumer_secret) . '&' . rawurlencode($oauth_access_token_secret);
-			$oauth_signature = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true));
-			$oauth['oauth_signature'] = $oauth_signature;
+				if (!empty($entityUrl) || !empty($entityHash) || !empty($entityUser)) {
 
-			$header = array(buildAuthorizationHeader($oauth), 'Expect:');
-			
-			$options = array(
-				CURLOPT_HTTPHEADER => $header,
-        CURLOPT_HEADER => false,
-        CURLOPT_URL => $url,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_SSL_VERIFYPEER => false
-      );
-
-      $feed = curl_init();
-      curl_setopt_array($feed, $options);
-      $json = curl_exec($feed);
-      curl_close($feed);
-      
-      $twitter_data = json_decode($json);
-      
-      if (isset($twitter_data)) {
-	        
-        	$tweets = theNest($twitter_data);
-        	
-					$output = array(
-						"tweets" => array(),
-						"user" => array()
-					);
-					
-					foreach ($tweets as $tweet) {
-					
-						$tweetText = $tweet['text'];
-							
-							if (isset($tweet['entities'])) {
-								if (is_array($tweet['entities']['urls'])) { $entityUrl = $tweet['entities']['urls']; }
-								if (is_array($tweet['entities']['hashtags'])) { $entityHash = $tweet['entities']['hashtags']; }
-								if (is_array($tweet['entities']['user_mentions'])) { $entityUser = $tweet['entities']['user_mentions']; }	
-							}
-							
-							if (!empty($entityUrl) || !empty($entityHash) || !empty($entityUser)) {
-							
-								foreach ($entityUrl as $url) {
-									$find = $url['url'];
-									$replace = '<a href="'.$find.'">'.$url['display_url'].'</a>';
-									$tweetText = str_replace($find,$replace,$tweetText);
-								}
-							                  
-								foreach ($entityHash as $hashtag) {
-									$find = '#'.$hashtag['text'];
-									$replace = '<a href="http://twitter.com/#!/search/%23'.$hashtag['text'].'">'.$find.'</a>';
-									$tweetText = str_replace($find,$replace,$tweetText);
-								}
-							                  
-								foreach ($entityUser as $user_mention) {
-									$find = "@".$user_mention['screen_name'];
-									$replace = '<a href="http://twitter.com/'.$user_mention['screen_name'].'">'.$find.'</a>';
-									$tweetText = str_replace($find,$replace,$tweetText);
-								}
-							
-							}
-							
-							if (!empty($tweet['entities']['media'])) {
-								foreach ($tweet['entities']['media'] as $media) {
-									$find = $media['url'];
-									$replace = '<a href="'.$media['url'].'">'.$media['display_url'].'</a>';
-									$tweetText = str_replace($find,$replace,$tweetText);
-								}
-							}
-											
-						$tweet['text'] = $tweetText;
-						$tweet['tweet_url'] = "https://twitter.com/" . $tweet['user']['screen_name'] . "/status/" . $tweet['id'];
-				
-						array_push($output["tweets"], $tweet);
-						array_push($output["user"], $tweet['user']);
-						
-					}				
-					
-					
-					for ($i = 1; $i <= $count; $i++) {
-						unset($output["user"][$i]);
+					foreach ($entityUrl as $url) {
+						$find = $url['url'];
+						$replace = '<a href="'.$find.'">'.$url['display_url'].'</a>';
+						$tweetText = str_replace($find, $replace, $tweetText);
 					}
-					
-					//birdTurd($output);
-	
-        	return $output;
-        
-        }
-        return false;
-      
-    
-    }
+
+					foreach ($entityHash as $hashtag) {
+						$find = '#'.$hashtag['text'];
+						$replace = '<a href="http://twitter.com/#!/search/%23'.$hashtag['text'].'">'.$find.'</a>';
+						$tweetText = str_replace($find, $replace, $tweetText);
+					}
+
+					foreach ($entityUser as $user_mention) {
+						$find = "@".$user_mention['screen_name'];
+						$replace = '<a href="http://twitter.com/'.$user_mention['screen_name'].'">'.$find.'</a>';
+						$tweetText = str_replace($find, $replace, $tweetText);
+					}
+
+				}
+
+				if (!empty($tweet['entities']['media'])) {
+					foreach ($tweet['entities']['media'] as $media) {
+						$find = $media['url'];
+						$replace = '<a href="'.$media['url'].'">'.$media['display_url'].'</a>';
+						$tweetText = str_replace($find, $replace, $tweetText);
+					}
+				}
+
+				$tweet['text'] = $tweetText;
+				$tweet['tweet_url'] = "https://twitter.com/" . $tweet['user']['screen_name'] . "/status/" . $tweet['id'];
+
+				array_push($output["tweets"], $tweet);
+				array_push($output["user"], $tweet['user']);
+
+			}
+
+
+			for ($i = 1; $i <= $count; $i++) {
+				unset($output["user"][$i]);
+			}
+
+			//birdTurd($output);
+
+			return $output;
+
+		}
+		return false;
+
+
+	}
 }
 
 function birdTurd($turd) {
@@ -173,8 +177,3 @@ function theNest($d) {
 		return $d;
 	}
 }
-
-
-
-
-
